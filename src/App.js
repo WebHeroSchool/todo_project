a1f65f5cedfc4ad56437a3b2a0642f98c8ddd789
Владимирова Екатерина <echapyrkina@mail.ru>
2020-11-17 17:34:32 +0400

    new items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 const ItemList = () => (<ul>
-	<li>1</li>
-	<li>2</li>
+	<li> React-компонент </li>
+	<li> Структура проекта </li>
+	<li> Props </li>
+	<li> React Developer Tools </li>
+	<li> Вывод списка элементов и ключи </li>
 	</ul>);
 const App = () => (<div>
-<h1> Hello World! </h1>
+<h1> Изучаем React </h1>
+<p> План работы на неделю </p>
 <ItemList />
 </div>);
 
