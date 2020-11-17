24224b5de1ed261ccd437d8ea3b01e2ad8c215bb
Владимирова Екатерина <echapyrkina@mail.ru>
2020-11-17 12:35:20 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './number';
+import React from 'react';
+const element = (<div>
+  Hello World!
+  <a href=""> go to </a>
+  </div>);
 
-class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style = {{
-        	color: 'red',
-        	fontSize: 20
-        }}> 
-        count * length = { count*length }</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    );
-  }
-}
-
-export default App;
+export { element };
--- src/number.js
+++
@@ -1,3 +0,0 @@
-const count = 20;
-const length = 9;
-export { count, length };
