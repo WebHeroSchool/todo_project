74cca6158e51aa449f75b5e1baaceaaf3cdcbda1
Владимирова Екатерина <echapyrkina@mail.ru>
2020-11-18 13:44:39 +0400

    new files

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+
+const App = () => (
+	<div>
+	<h1> План обучения на неделю </h1>
+	<ItemList />
+	</div>);
+
+	export default App;
---
+++ src/components/Item/Item.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const Item = () => (<span> Изучение урока по React </span>);
+
+	export default Item;
