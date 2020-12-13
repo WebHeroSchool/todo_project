cfb00154cc6ad26fb2345742c3f2a3d661730d9e
Владимирова Екатерина <echapyrkina@mail.ru>
2020-12-13 16:42:45 +0400

    правки

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import InputItem from '../InputItem/InputItem';
