110e578b0b4148754ab4ae848eec683b8cfda522
Владимирова Екатерина <echapyrkina@mail.ru>
2021-02-06 13:21:13 +0400

    добавлена ссылка на школу

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Logo.module.css';
+import logo from '../../img/logo.png';
 
 const Logo = () => (
 	<div className={styles.logo}>
 	<div className={styles.develop}>Разработано в</div>
-	<div className={styles.hero}>WebHero</div>
-	<div className={styles.school}>School</div>
+	<a href="https://webheroschool.ru/" target="__blank" rel="noopener noreferer">
+	<img src={logo} alt="schoollogo"></img>
+	</a>
 	</div>
 )
 
